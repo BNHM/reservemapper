50d0ec028a4f5f7685f435b8da56ade41736044d
John Deck <jdeck88@gmail.com>
2018-02-12 14:59:17 -0800

    getting status to a point where it works

--- app/query/stats/queryStats.controller.js
+++ app/query/stats/queryStats.controller.js
@@ -15,12 +15,41 @@
         //var totalResults = vm.totalResult;
         vm.queryResults = queryResults;
 
+	$scope.gridOptions = {
+            data: []
+        };
+
 	// Watch for when the queryResults size changes, then run this function
      	$scope.$watch('queryStatsVm.queryResults.size', function () {
           vm.totalResults=vm.queryResults.totalElements
           vm.total
+          $scope.gridOptions.data = valueTotal('year', 'value', 'ascending')
      	});
-    }
 
-
+// Group on a name and return the number of counts for each name in the dataset
+// parameters are:
+// 2. a name containing an attribute in the JSON Object
+// 3. sortTopic "key" or "value"
+// 4. sortDirection "ascending" or "descending"
+// 5. nestedName, another name to nest
+function valueTotal(name, sortTopic, sortDirection, nestedName) {
+        var groupData;
+        if (nestedName != null) {
+                groupData = d3.nest()
+                .key(function(d) { return eval('d.'+name); })
+                .key(function(d) { return eval('d.'+nestedName); })
+                .rollup(function(v) {return v.length; })
+                .entries(vm.queryResults.data)
+        } else {
+                groupData = d3.nest()
+                .key(function(d) { return eval('d.'+name); })
+                .rollup(function(v) {return v.length; })
+                .entries(vm.queryResults.data)
+        }
+        // sort by key,value and ascending,descending
+        return groupData.sort(function(x,y) {
+                return eval('d3.'+sortDirection+'(x.'+sortTopic+',y.'+sortTopic+')');
+        });
+}
+}
 })();
