44ff473019e69ff84705f48037a1fbcfb5399c5e
John Deck <jdeck88@gmail.com>
2020-01-27 19:38:07 -0800

    cleaning up files, simplifying gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,19 @@ var replace = require('gulp-replace');
 var rimraf = require("rimraf");
 
 
-
-function fonts() {
-    return gulp
-    	.src('app/bower_components/bootstrap/dist/fonts/**/*')
-	.pipe(gulp.dest("public/fonts/"));
-}
-function cssimages() {
-    return gulp
-    	.src('app/bower_components/leaflet/dist/images/**/*')
-	.pipe(gulp.dest("public/css/images/"));
-}
-function images() {
-    return gulp
-    	.src('app/images/*.png')
-	.pipe(gulp.dest("public/images"));
-}
-
 function core() {
     return gulp
-    .src(['app/**/*.html', 'app/**/*.ico', '!app/index-async.html', '!app/bower_components/**/*.html', 'app/**/*'])
-        .pipe(gulp.dest('public'))
-        .pipe(replace('<base href="/">', '<base href="/">'))
+	.src(['app/**/*', '!app/index-async.html', '!app/bower_components/**/*'])
+	.pipe(gulp.dest('public'))
+	.pipe(replace('<base href="/">', '<base href="/">'))
 }
 
 // Clean assets
 function clean(cb) {
-  return rimraf("./public/",cb);
+    return rimraf("./public/",cb);
 }
 
-const build = gulp.series(core, images, cssimages, fonts);
+const build = gulp.series(core)
 
 // export tasks
 exports.default = build;
